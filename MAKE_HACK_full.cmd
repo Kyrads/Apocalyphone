6d92f6979c721d04783d77d5376135fad0b6f010
Ditto <myriadarrows0@gmail.com>
2026-07-12 15:34:18 +0100

    Shut up a bunch of warnings that we can ignore
    (SetSymbolMacros error ignore done by editing ColorzCore)

--- MAKE_HACK_full.cmd
+++ MAKE_HACK_full.cmd
@@ -61,7 +61,7 @@ echo:
 echo Assembling
 
 cd "%base_dir%EventAssembler"
-ColorzCore A FE8 "-output:%target_rom%" "-input:%main_event%" --nocash-sym
+ColorzCore A FE8 "-output:%target_rom%" "-input:%main_event%" --nocash-sym --warnings:no-set-symbol-macros
 
 if /I not [%1]==[quick] (
 
